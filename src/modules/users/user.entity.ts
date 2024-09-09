2b6163272d81e861bb572b4557e3dc8f49f12b43
Yves Francisco <yvesfrancisco95@gmail.com>
2024-09-10 01:49:57 +0800

    feat: Decorated user entity with mikro-orm decorators.

--- src/modules/users/user.entity.ts
+++ src/modules/users/user.entity.ts
@@ -1,6 +1,11 @@
 import { BaseEntity } from '@/common/entities';
+import { Entity, Property } from '@mikro-orm/core';
 
+@Entity({ collection: 'users' })
 export class User extends BaseEntity {
+  @Property({ unique: true })
   email: string;
+
+  @Property()
   password: string;
 }
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,8 +1,11 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [MikroOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
 })
