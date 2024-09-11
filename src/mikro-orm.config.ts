a5ed5f6eb96af1f919752324f6713cda564b277b
Yves Francisco <yvesfrancisco95@gmail.com>
2024-09-11 16:19:03 +0800

    feat: Setup mikro-orm logger.

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from '@mikro-orm/core';
 import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { SeedManager } from '@mikro-orm/seeder';
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
+import { Logger } from '@nestjs/common';
 import { BaseRepository } from './common/db';
 import { config } from './config';
+
+const logger = new Logger('MikroORM');
 
 const kebabize = (str: string) =>
   str.replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? '-' : '') + $.toLowerCase());
@@ -32,4 +36,8 @@ export default defineConfig({
   password: config.db.password,
   /** Debug mode of the database */
   debug: config.db.debug,
+  /** Library used to highlight various things in the CLI, like SQL and mongo queries, or migrations or entities generated via CLI. */
+  highlighter: new SqlHighlighter(),
+  /** Logger to use by the ORM. */
+  logger: (msg) => logger.debug(msg),
 });
